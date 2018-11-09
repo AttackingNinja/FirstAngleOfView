e74b17b4b8ba71e5cde26b1faae01925e232772f
Lee_Cooper <495203870@qq.com>
2018-11-09 12:47:04 +0800

    walkModeButton_Development
    including walkModeBUtton UI design and  JS implement.

--- public/javascripts/FirstAngleOfView.js
+++ public/javascripts/FirstAngleOfView.js
@@ -3,80 +3,52 @@ var viewer = new Cesium.Viewer('cesiumContainer');
 var scene = viewer.scene;
 var canvas = scene.canvas;
 var ellipsoid = viewer.scene.globe.ellipsoid;
-/*function getPosition()
-{
-    //得到当前三维场景
-    var scene = viewer.scene;
-    //得到当前三维场景的椭球体
-    var ellipsoid = scene.globe.ellipsoid;
-    var entity = viewer.entities.add(
-        {
-        label : {
-            show : false
-        }
-    });
-    var longitudeString = null;
-    var latitudeString = null;
-    var height = null;
-    var cartesian = null;
-    // 定义当前场景的画布元素的事件处理
-    var handler = new Cesium.ScreenSpaceEventHandler(scene.canvas);
-    //设置鼠标移动事件的处理函数，这里负责监听x,y坐标值变化
-    handler.setInputAction(function(movement)
-    {
-        //通过指定的椭球或者地图对应的坐标系，将鼠标的二维坐标转换为对应椭球体三维坐标
-        cartesian = viewer.camera.pickEllipsoid(movement.endPosition, ellipsoid);
-        if (cartesian)
-        {
-            //将笛卡尔坐标转换为地理坐标
-            var cartographic = ellipsoid.cartesianToCartographic(cartesian);
-            //将弧度转为度的十进制度表示
-            longitudeString = Cesium.Math.toDegrees(cartographic.longitude);
-            latitudeString = Cesium.Math.toDegrees(cartographic.latitude);
-            //获取相机高度
-            height = Math.ceil(viewer.camera.positionCartographic.height);
-            entity.position = cartesian;
-            entity.label.show = true;
-            entity.label.text = '(' + longitudeString + ', ' + latitudeString + "," + height + ')' ;
-        }
-        else {
-            entity.label.show = false;
-        }
-    }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
-    //设置鼠标滚动事件的处理函数，这里负责监听高度值变化
-    handler.setInputAction(function(wheelment) {
-        height = Math.ceil(viewer.camera.positionCartographic.height);
-        entity.position = cartesian;
-        entity.label.show = true;
-        entity.label.text = '(' + longitudeString + ', ' + latitudeString + "," + height + ')' ;
-    }, Cesium.ScreenSpaceEventType.WHEEL);
-}*/
-function onClick()
-{
-    var handler = new Cesium.ScreenSpaceEventHandler(canvas);
-    handler.setInputAction(function (movement)
-    {
-        var cartesian = viewer.camera.pickEllipsoid(movement.position,ellipsoid);
-        if(cartesian)
-        {
+var handler = new Cesium.ScreenSpaceEventHandler(canvas);
+var walkModeSwitch = false;
+
+function mouseControlSwitch(setSwitch) {
+    if (setSwitch == true) {
+        scene.screenSpaceCameraController.enableRotate = true;
+        scene.screenSpaceCameraController.enableTranslate = true;
+        scene.screenSpaceCameraController.enableZoom = true;
+        scene.screenSpaceCameraController.enableTilt = true;
+        scene.screenSpaceCameraController.enableLook = true;
+    }
+    else if (setSwitch == false) {
+        scene.screenSpaceCameraController.enableRotate = false;
+        scene.screenSpaceCameraController.enableTranslate = false;
+        scene.screenSpaceCameraController.enableZoom = false;
+        scene.screenSpaceCameraController.enableTilt = false;
+        scene.screenSpaceCameraController.enableLook = false;
+    }
+}
+
+function walkModeOnClick() {
+    handler.setInputAction(function (movement) {
+        var cartesian = viewer.camera.pickEllipsoid(movement.position, ellipsoid);
+        if (cartesian) {
             var cartographic = ellipsoid.cartesianToCartographic(cartesian);
             viewer.camera.flyTo(
                 {
                     destination: Cesium.Cartesian3.fromRadians(cartographic.longitude, cartographic.latitude, 1.6),
-                    orientation:{
-                        heading : viewer.camera.heading,
-                        pitch : Cesium.Math.toRadians(0),
-                        roll:viewer.camera.roll
+                    orientation: {
+                        heading: viewer.camera.heading,
+                        pitch: Cesium.Math.toRadians(0),
+                        roll: viewer.camera.roll
                     },
-                    duration:1
+                    duration: 1
                 }
             );
-            scene.screenSpaceCameraController.enableRotate = false;
-            scene.screenSpaceCameraController.enableTranslate = false;
-            scene.screenSpaceCameraController.enableZoom = false;
-            scene.screenSpaceCameraController.enableTilt = false;
-            scene.screenSpaceCameraController.enableLook = false;
-            document.addEventListener('keydown',function (e) {
+            if (walkModeSwitch == false) {
+                walkModeSwitch = true;
+                document.getElementById("walkModeButton").style.color = "#fff";
+                document.getElementById("walkModeButton").style.fill = "#fff";
+                document.getElementById("walkModeButton").style.background = "#48b";
+                document.getElementById("walkModeButton").style.borderColor = "#aef";
+                document.getElementById("walkModeButton").style.boxShadow = "0 0 8px #fff";
+                document.getElementById("cesiumContainer").style.cursor = "default";
+            }
+            document.addEventListener('keydown', function (e) {
                 switch (e.keyCode) {
                     case 87://W
                         viewer.camera.moveForward(1);
@@ -100,12 +72,36 @@ function onClick()
                         viewer.camera.moveUp(0.1);
                         break;
                     case 88://X
-                        if(viewer.camera.positionCartographic.height>=1.7)
+                        if (viewer.camera.positionCartographic.height >= 1.7)
                             viewer.camera.moveDown(0.1);
                         break;
                 }
             })
         }
-    },Cesium.ScreenSpaceEventType.LEFT_CLICK);
+    }, Cesium.ScreenSpaceEventType.LEFT_CLICK);
 }
-onClick();
+
+function setWalkModeButtonOnStyle() {
+    if (walkModeSwitch == false) {
+        document.getElementById("cesiumContainer").style.cursor = "url(/images/walkManCursor.ico),auto";
+        walkModeOnClick(walkModeSwitch);
+    }
+    else {
+        walkModeSwitch = false;
+        handler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_CLICK);
+        var cartographic = ellipsoid.cartesianToCartographic(viewer.camera.positionWC);
+        viewer.camera.flyTo(
+            {
+                destination: Cesium.Cartesian3.fromRadians(cartographic.longitude, cartographic.latitude, 1000),
+                duration: 1
+            }
+        );
+        document.getElementById("walkModeButton").style.color = "rgb(255, 255, 255)";
+        document.getElementById("walkModeButton").style.fill = "none"
+        document.getElementById("walkModeButton").style.background = "#303336";
+        document.getElementById("walkModeButton").style.borderColor = "none";
+        document.getElementById("walkModeButton").style.boxShadow = "rgba(0, 0, 0, 0.14) 0px 2px 2px 0px, rgba(0, 0, 0, 0.2) 0px 3px 1px -2px, rgba(0, 0, 0, 0.12) 0px 1px 5px 0px";
+        document.getElementById("cesiumContainer").style.cursor = "default";
+    }
+}
+
